148a902a56f4dbde35d8608d3b6d933ef7e6454a
Aleksandr2905 <natalukha@ukr.net>
2023-10-09 00:35:12 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,45 +1,41 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 
-export class Searchbar extends Component {
-  state = {
-    tags: '',
-  };
+export const Searchbar = ({ onSubmit }) => {
+  const [tags, setTags] = useState('');
 
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const searchTerm = event.target.value.toLowerCase();
-    this.setState({ tags: searchTerm });
+    setTags(searchTerm);
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state.tags);
-    if (!this.state.tags.trim() === '') {
+    onSubmit(tags);
+    if (!tags.trim() === '') {
       toast.error('Please write a word for your request');
       return;
     }
-    this.setState({ tags: '' });
+    setTags('');
   };
 
-  render() {
-    return (
-      <header className="Searchbar">
-        <form className="SearchForm" onSubmit={this.handleSubmit}>
-          <button type="submit" className="SearchForm-button">
-            <span className="SearchForm-button-label">Search</span>
-          </button>
+  return (
+    <header className="Searchbar">
+      <form className="SearchForm" onSubmit={handleSubmit}>
+        <button type="submit" className="SearchForm-button">
+          <span className="SearchForm-button-label">Search</span>
+        </button>
 
-          <input
-            className="SearchForm-input"
-            type="text"
-            autocomplete="off"
-            autofocus
-            placeholder="Search images and photos"
-            value={this.state.tags}
-            onChange={this.handleInputChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className="SearchForm-input"
+          type="text"
+          autocomplete="off"
+          autofocus
+          placeholder="Search images and photos"
+          value={tags}
+          onChange={handleInputChange}
+        />
+      </form>
+    </header>
+  );
+};
